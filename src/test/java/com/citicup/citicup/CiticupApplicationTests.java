689f3a93d420c3cac759db2c88489879994e93f4
山山 <181250019@smail.nju.edu.cn>
2020-09-04 09:39:01 +0800

    feat: 后端框架

---
+++ src/test/java/com/citicup/citicup/CiticupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citicup.citicup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiticupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
