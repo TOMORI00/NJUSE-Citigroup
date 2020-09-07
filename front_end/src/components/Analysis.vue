3d881cc62c8a214748abd41b7ffb2306b92070dc
SunriseToSunset <48702884+noGoodnight@users.noreply.github.com>
2020-09-07 08:59:28 +0800

    feat: 建立文件以及路由跳转

---
+++ front_end/src/components/Analysis.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Analysis</p>
+</template>
+
+<script>
+export default {
+  name: "Analysis"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ front_end/src/views/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-divider>HomePage</el-divider>
+    <div>
+      <el-button @click="toAdvanced">Advanced</el-button>
+      <el-button @click="toNormal">Normal</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  methods: {
+    toAdvanced() {
+      this.$router.replace('/advanced')
+    },
+    toNormal() {
+      this.$router.replace('/normal')
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
