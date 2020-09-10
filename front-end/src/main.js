e42b532abed7fa9ccaba35b0a92ca60bd0ba40db
山山 <181250019@smail.nju.edu.cn>
2020-09-10 13:14:39 +0800

    feat: 添加表格代码

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,11 +7,13 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import VueGoogleCharts from 'vue-google-charts';
 
 Vue.config.productionTip = false
 
 Vue.use(Antd)
 Vue.use(ElementUI)
+Vue.use(VueGoogleCharts)
 
 new Vue({
   router,
--- front-end/src/views/Normal.vue
+++ front-end/src/views/Normal.vue
@@ -4,16 +4,39 @@
     <el-divider></el-divider>
     <el-button>一键生成报告</el-button>
     <el-button @click="toHome">HomePage</el-button>
+    <GChart 
+      type="LineChart"
+      :data="chartData"
+      :options="chartOption"/>
   </div>
 </template>
 
 <script>
 import Recommend from "@/components/Recommend";
+import {GChart} from 'vue-google-charts';
 
 export default {
   name: "Normal",
+  data(){
+    return {
+      chartData:[
+        ['x-line','number1','number2'],
+        [20,25,30],
+        [25,40,56],
+        [30,56,24]
+      ],
+      //chartOption控制表格属性，更多选项请参考官方API：“https://developers.google.cn/chart/interactive/docs/gallery/linechart?hl=en#configuration-options”
+      chartOption:{
+        charts:{
+          title:'testChart'
+        },
+        focusTarget:'category'
+      }
+    }
+  },
   components: {
-    Recommend
+    Recommend,
+    GChart
   },
   methods:{
     toHome(){
